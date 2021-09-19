8d99495a3a6d5f183d1e67254179c74605dfa5d9
Vadim <gabderahmanov99@gmail.com>
2021-09-19 13:24:04 +0300

    Initial commit

---
+++ app/src/main/java/com/vad/methoddevidelinefifty/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vad.methoddevidelinefifty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextFunction;
+    private EditText editTextA;
+    private EditText editTextB;
+    private EditText editTextEpsilon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextFunction = (EditText) findViewById(R.id.editTextFunction);
+        editTextA = (EditText) findViewById(R.id.editTextNumberA);
+        editTextB = (EditText) findViewById(R.id.editTextNumberB);
+        editTextEpsilon = (EditText) findViewById(R.id.editTextNumberEpsilon);
+
+
+    }
+}
