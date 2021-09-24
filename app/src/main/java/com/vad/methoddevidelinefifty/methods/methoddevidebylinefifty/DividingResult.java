aa61a491846358c0a3d32482b0ec5afb49c1562d
Vadim <gabderahmanov99@gmail.com>
2021-09-24 11:57:03 +0300

    add presenter for method deviding

---
+++ app/src/main/java/com/vad/methoddevidelinefifty/methods/methoddevidebylinefifty/DividingResult.java
@@ -0,0 +1,111 @@
+package com.vad.methoddevidelinefifty.methods.methoddevidebylinefifty;
+
+public class DividingResult {
+    private float ak;
+    private float bk;
+
+    private int k;
+
+    private float xck;
+    private float l;
+
+    private float fFromXck;
+    private float fFromZk;
+    private float fFromYk;
+
+    private float yk;
+    private float zk;
+
+    public DividingResult(float ak, float bk, int k, float xck, float l, float fFromXck, float fFromZk, float fFromYk, float yk, float zk) {
+        this.ak = ak;
+        this.bk = bk;
+        this.k = k;
+        this.xck = xck;
+        this.l = l;
+        this.fFromXck = fFromXck;
+        this.fFromZk = fFromZk;
+        this.fFromYk = fFromYk;
+        this.yk = yk;
+        this.zk = zk;
+    }
+
+    public float getAk() {
+        return ak;
+    }
+
+    public void setAk(float ak) {
+        this.ak = ak;
+    }
+
+    public float getBk() {
+        return bk;
+    }
+
+    public void setBk(float bk) {
+        this.bk = bk;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public float getXck() {
+        return xck;
+    }
+
+    public void setXck(float xck) {
+        this.xck = xck;
+    }
+
+    public float getL() {
+        return l;
+    }
+
+    public void setL(float l) {
+        this.l = l;
+    }
+
+    public float getfFromXck() {
+        return fFromXck;
+    }
+
+    public void setfFromXck(float fFromXck) {
+        this.fFromXck = fFromXck;
+    }
+
+    public float getfFromZk() {
+        return fFromZk;
+    }
+
+    public void setfFromZk(float fFromZk) {
+        this.fFromZk = fFromZk;
+    }
+
+    public float getfFromYk() {
+        return fFromYk;
+    }
+
+    public void setfFromYk(float fFromYk) {
+        this.fFromYk = fFromYk;
+    }
+
+    public float getYk() {
+        return yk;
+    }
+
+    public void setYk(float yk) {
+        this.yk = yk;
+    }
+
+    public float getZk() {
+        return zk;
+    }
+
+    public void setZk(float zk) {
+        this.zk = zk;
+    }
+}
