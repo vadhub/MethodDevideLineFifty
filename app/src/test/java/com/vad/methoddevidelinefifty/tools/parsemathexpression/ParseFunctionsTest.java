9110a1b8cbc48b9c8be9502cfc0482b32d80ec51
Vadim <gabderahmanov99@gmail.com>
2021-09-27 15:43:11 +0300

    add test lex analyse

--- app/src/test/java/com/vad/methoddevidelinefifty/tools/parsemathexpression/ParseFunctionsTest.java
+++ app/src/test/java/com/vad/methoddevidelinefifty/tools/parsemathexpression/ParseFunctionsTest.java
@@ -4,7 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class ParseFunctionsTest {
+
+    @Test
+    void lexAnalise_sum() {
+        List<Lexeme> lexemes = ParseFunctions.lexAnalise("2+2");
+
+        Lexeme expect = lexemes.get(0);
+        assertEquals(LexemeType.NUMBER, expect.lexemeType);
+        assertEquals("2", expect.value);
+
+        expect = lexemes.get(1);
+        assertEquals(LexemeType.OP_PLUS, expect.lexemeType);
+        assertEquals("+", expect.value);
+
+        expect = lexemes.get(2);
+        assertEquals(LexemeType.NUMBER, expect.lexemeType);
+        assertEquals("2", expect.value);
+    }
 
     @Test
     void parseExpression_plus() {
@@ -49,6 +68,15 @@ class ParseFunctionsTest {
     }
 
     @Test
-    void lexAnalise() {
+    void parseExpression_functions() {
+        ParseFunctions parseFunctions = new ParseFunctions();
+        float expect = parseFunctions.parseExpression("pow(2,2)");
+        assertEquals(4, expect);
+
+        expect = parseFunctions.parseExpression("sin(60)");
+        assertEquals((float)Math.sin(Math.toRadians(60)), expect);
+
+        expect = parseFunctions.parseExpression("ln(10)");
+        assertEquals((float)Math.log(10), expect);
     }
 }
